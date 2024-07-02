8855d1e7d11e8ad2ada1234f1bfb01d780dd076d
rorrocatalam <rodrigo.catalan.b@ug.uchile.cl>
2024-07-02 17:00:50 -0400

    Modifico imu.py para que guarde la informacion en un archivo txt cada 1 segundo

--- imu.py
+++ imu.py
@@ -1,17 +1,21 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
 import time
 import board
 import adafruit_icm20x
 
-i2c = board.I2C()  # uses board.SCL and board.SDA
-# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+i2c = board.I2C()
 icm = adafruit_icm20x.ICM20948(i2c)
 
+output_file = "sensor_data.txt"  # Nombre del archivo donde se guardarán los datos
+
 while True:
-    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (icm.acceleration))
-    print("Gyro X:%.2f, Y: %.2f, Z: %.2f rads/s" % (icm.gyro))
-    print("Magnetometer X:%.2f, Y: %.2f, Z: %.2f uT" % (icm.magnetic))
-    print("")
-    time.sleep(0.5)
+    acceleration = icm.acceleration
+    gyro = icm.gyro
+    magnetometer = icm.magnetic
+    
+    with open(output_file, "a") as file:
+        file.write(f"Acceleration: X:{acceleration[0]:.2f}, Y: {acceleration[1]:.2f}, Z: {acceleration[2]:.2f} m/s^2\n")
+        file.write(f"Gyro X:{gyro[0]:.2f}, Y: {gyro[1]:.2f}, Z: {gyro[2]:.2f} rads/s\n")
+        file.write(f"Magnetometer X:{magnetometer[0]:.2f}, Y: {magnetometer[1]:.2f}, Z: {magnetometer[2]:.2f} uT\n")
+        file.write("\n")
+
+    time.sleep(1)  # Espera de 1 segundo antes de tomar la siguiente lectura
